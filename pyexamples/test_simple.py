8fa3a676f148c940bf3ac4117ac29f591ec57879
Perri-673 <110961097+Perri-673@users.noreply.github.com>
2025-02-18 18:35:32 +0900

    Update test_simple.py

--- pyexamples/test_simple.py
+++ pyexamples/test_simple.py
@@ -94,7 +94,7 @@ def generate_architecture():
 
 def main():
     arch = generate_architecture()
-    namefile = "generator_architecture"
+    namefile = "test_simple.tex"
     to_generate(arch, namefile + '.tex')
 
 if __name__ == '__main__':
